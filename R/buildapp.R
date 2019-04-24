9c4b48677c11b2944aad774581f6ec9f975e0af1
Andy McCarthy <andrew.mccarthy@theperducogroup.com>
2019-04-24 11:58:27 -0400

    tweak to DESCRIPTION version

---
+++ R/buildapp.R
@@ -0,0 +1,15 @@
+#' @title Create App
+#'
+#' @description series of functions to actually wrap in electron and write installer
+#'
+#' @export
+
+buildapp <- function() {
+  # create electron-wrapped application
+  RInno::create_app(
+    app_name     = "NBADraft",
+    app_repo_url = "https://github.com/Andy-McCarthy/NBADraft",
+    pkgs         = c("magrittr", "httr", "shiny", "myapp"))
+  # write installer
+  RInno::compile_iss()
+}
